8254731bbd20442d3d5b4bd55efaad333b581cfb
andrekevinn <andrekevinn@gmail.com>
2025-04-29 18:02:17 -0300

    Entrada e Saída

--- 2025-04-29/EeS.py
+++ 2025-04-29/EeS.py
@@ -1,1 +1,11 @@
 print('calcular área do círculo')
+
+raio = 5.17
+
+área = 3.14 * raio ** 2
+
+print('área do círculo:', área)
+
+print('A área do círculo de raio', raio, 'é', área)
+
+print(f'A área do círculo de raio {raio} é {área:.4f}')
