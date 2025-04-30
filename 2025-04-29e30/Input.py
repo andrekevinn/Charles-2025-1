5befa86ebbf8202ad991fd6f241a2badd79056b3
andrekevinn <andrekevinn@gmail.com>
2025-04-30 17:36:00 -0300

    cuida pra cuidar

---
+++ 2025-04-29e30/Input.py
@@ -0,0 +1,11 @@
+# Programa para calcular área do círculo
+
+raio = float(input('Digite o raio do círculo: '))
+
+area = 3.14 * raio ** 2
+
+print(f'A área do círculo é: {area:.2f}')
+
+comprimento = 2 * 3.14 * raio
+
+print(f'O comprimento da circunferência e: {comprimento:.2f}')
---
+++ 2025-04-29e30/MediaIFRN.py
@@ -0,0 +1,13 @@
+# Calcular média da disciplina do IFRN
+
+# 1. Solicitar ao usuário que informe duas notas
+# 2. Calcular a média do IFRN
+# 3. Exibir a média
+
+Etapa1 = float(input('Digite a nota da Etapa1: '))
+
+Etapa2 = float(input('Digite a nota da Etapa2: '))
+
+Média = ((Etapa1 * 2) + (Etapa2 * 3)) / 5
+
+print(f'A Média é: {Média:.0f}')
