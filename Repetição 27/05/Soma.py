d6f6c877beb24fb4a503b8f204901e0a4ad32fc8
andrekevinn <andrekevinn@gmail.com>
2025-05-27 20:45:28 +0000

    Questão 1

---
+++ Repetição 27/05/Soma.py
@@ -0,0 +1,16 @@
+'''Programa para executar um produto de 2 número inteiros positvos utilizando apenas o operador soma (+)'''
+import sys
+try:
+    n1 = int(input('Insira um número inteiro: '))
+    n2 = int(input('Insira um número inteiro para multiplicar o primeiro: '))
+    if n1 <= 0: 
+        sys.exit('ERRO: Insira um valor positivo.')
+    if n2 <= 0: 
+        sys.exit('ERRO: Insira um valor positivo.')
+except ValueError: sys.exit('ERRO: Insira um valor que possa ser convertido para inteiro')
+soma = n2 + n2
+while n1 <= n2: 
+    print(f'{soma}')
+    soma += n2
+else:
+    print('fim')
