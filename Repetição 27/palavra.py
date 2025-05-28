5b16089b10ce9ea7737d1fe519dc1ad4245c70f7
andrekevinn <andrekevinn@gmail.com>
2025-05-28 20:11:57 +0000

    Senha

--- Repetição 27/palavra.py
+++ Repetição 27/palavra.py
@@ -1,1 +1,9 @@
-'''Senha repitagit'''
+'''Senha repetida'''
+import sys
+PC = '123Mudar'
+PI = None
+
+while PI != PC:
+    PI = input('Digite a palavra chave: ')
+
+print('Você digitou a palavra chave.')
