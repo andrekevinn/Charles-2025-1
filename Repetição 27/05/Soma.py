3a490af59fa6da456284f8362a9607d9a97e7f17
andrekevinn <andrekevinn@gmail.com>
2025-05-28 19:56:43 +0000

    Soma

--- Repetição 27/05/Soma.py
+++ Repetição 27/05/Soma.py
@@ -3,13 +3,17 @@ import sys
 try:
     n1 = int(input('Insira um número inteiro: '))
     n2 = int(input('Insira um número inteiro para multiplicar o primeiro: '))
+except ValueError: sys.exit('ERRO: Insira um valor que possa ser convertido para inteiro')
+else:
     if n1 <= 0: 
         sys.exit('ERRO: Insira um valor positivo.')
     if n2 <= 0: 
         sys.exit('ERRO: Insira um valor positivo.')
-except ValueError: sys.exit('ERRO: Insira um valor que possa ser convertido para inteiro')
-while n1 <= n2: 
-    print(f'{n1} + {n2} =  {n1 + n2}')
-    + n2
-else:
-    print('fim')
+
+    produto = 0
+    contador = 1
+    while contador <= n2:
+        produto += n1
+        contador += 1
+
+    print(f'{n1} x {n2} = {produto}')
